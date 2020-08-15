8499f1a6b90471510ed9902f07f23d7a5019c755
Aaron <euaaron@outlook.com>
2020-08-15 01:56:13 -0300

    Server and front changes - added subdomain 'api' for server; - changed all colors, all, including from .svg files; - fixed landing page grid; - created a forder for readme media files; - added header.png to replace readme title.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
-import  express from 'express';
-import cors from 'cors';
+import express    from 'express';
+import subdomain  from 'express-subdomain';
+import cors       from 'cors';
+
 import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use(subdomain('api', routes));
 
 app.listen(3333);
